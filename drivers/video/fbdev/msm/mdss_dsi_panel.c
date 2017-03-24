231c0a62482366da61112fe9a95a59462e1a5e61
Francisco Franco <franciscofranco.1990@gmail.com>
2017-03-24 15:02:38 +0000

    display: Add a noob and bad api to query the display state
    Adjusted for OP5 (msm8998) by @xdevs23
    
    Why do you do this franco??? You could use FB instead.
    
    Change-Id: I9e803eb416d1c22afa820d847286e43e740b70a5
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -34,11 +34,19 @@
 #include "mdss_dsi_iris2p_lightup.h"
 #include <linux/project_info.h>
 
+#include <linux/display_state.h>
 
 #define DT_CMD_HDR 6
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
 #define VSYNC_DELAY msecs_to_jiffies(17)
+
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
 
@@ -1519,6 +1527,7 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
     pr_err("%s start\n", __func__);
+	display_on = true;
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1679,6 +1688,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		if (ctrl->off_cmds.cmd_cnt)
 			mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 	}
+
+	display_on = false;
 
 	if (ctrl->ds_registered && pinfo->is_pluggable) {
 		mdss_dba_utils_video_off(pinfo->dba_data);
