175348ba3eda64b58ca19aec6efd6a0ab8242d50
joshuous <joshuous@gmail.com>
2017-08-27 06:46:01 +0000

    cpu-boost: Introduce Dynamic Stune Boost
    Dynamic schedtune boosting occurs on-the-fly during interactions, such as touch input. This idea was conceived because of an existing battery drain issue whereby setting a permanent schedtune boost would cause boosting to occur even when the phone was idling. For example, if a user sets the /dev/stune/top-app/schedtune.boost to 10, the device may not idle at the lowest frequency step, which can lead to higher idle and active drain rates. Thus it should be better on the battery to boost only during interactions.
    
    The tunable can be found in /sys/module/cpu_boost/parameters/dynamic_stune_boost.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -50,6 +50,13 @@ module_param(sched_boost_on_input, bool, 0644);
 
 static bool sched_boost_active;
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+static int dynamic_stune_boost = 0;
+module_param(dynamic_stune_boost, uint, 0644);
+
+int sched_dynamic_stune_boost = 0;
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 
@@ -180,6 +187,11 @@ static void do_input_boost_rem(struct work_struct *work)
 		i_sync_info->input_boost_min = 0;
 	}
 
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Reset dynamic stune boost value to lowest schedtune.boost value allowed */
+	sched_dynamic_stune_boost = -100;
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
 	/* Update policies for all online CPUs */
 	update_policy_online();
 
@@ -222,6 +234,11 @@ static void do_input_boost(struct work_struct *work)
 
 		i_sync_info->input_boost_min = i_sync_info->input_boost_freq;
 	}
+
+#ifdef CONFIG_DYNAMIC_STUNE_BOOST
+	/* Set dynamic stune boost value */
+	sched_dynamic_stune_boost = dynamic_stune_boost;
+#endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
